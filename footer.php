a078588f64b7a0196df8715d3042dad639919c38
ColinSidberry <39615668+ColinSidberry@users.noreply.github.com>
2018-05-30 23:09:52 -0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,186 @@
+</div>
+      <div id="modal_container"></div>
+    
+
+
+    
+
+<div id="grove--marketing--modal--tarp" class="grove--modal--tarp animate hide">
+  <div class="valign--container">
+    <div class="valign--middle">
+      <div class="marketing--bounds">
+        <!-- Success modal -->
+<div class="grid-max-75 centered">
+  <div id="grove--guide--modal--success" class="grove--modal guide--modal hide">
+    <a class="grove--modal--close"></a>
+
+    <div class="grid-40 grid-tablet-100">
+      <div class="guide--modal--pdf">
+        <img src="https://hellogrove.com/dist/img/marketing/guide-hero.png" class="guide--modal--image">
+        <h4 class="guide--modal--h4">2018 Personal Finance Guide</h4>
+        <p class="guide--modal--small">9 things you need to do to get your finances in order</p>
+      </div>
+    </div>
+
+    <div class="grid-60 grid-tablet-100">
+      <div class="guide--success">
+        <span class="icon-check"></span>
+        <div class="guide--success--message">
+          <h2 class="guide--success--message--h2">Thanks!</h2>
+          <p class="guide--success--message--p">Your guide is on its way.</p>
+        </div>
+        <div class="guide--success--checkup">
+          <h2 class="guide--success--checkup--h2">Get a FREE Financial Checkup</h2>
+          <p class="guide--success--checkup--p">We’ll evaluate 8+ areas of your finances to see how your money is doing</p>
+          <div class="guide--success--checkup--button">
+            <span data-component="StartAssessmentButton"
+                  category="assessment"
+                  action="click get started"
+                  label="pdf thank you"
+                  text="Start checkup">
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<!--  -->
+
+<!-- Initial newsletter signup modal -->
+<div class="grid-max-75 centered">
+  <div id="grove--guide--modal--prompt" class="grove--modal guide--modal hide">
+    <a class="grove--modal--close"></a>
+
+    <div class="grid-60 grid-tablet-100">
+      <div class="guide--modal--message">
+        <h3 class="guide--modal--h3">Sign up for Grove’s personal finance newsletter and get...</h3>
+        <h4 class="guide--modal--label">Free download</h4>
+        <h2 class="guide--modal--h2">The Guide to Your 2018 Personal Finances</h2>
+        <p class="guide--modal--p">PDF Guide</p>
+
+        <div data-component="NewsletterSignup" label="modal"
+          outerClassName="guide--modal--form" buttonText="Get the guide">
+        </div>
+      </div>
+    </div>
+
+    <div class="grid-40 grid-tablet-100">
+      <div class="guide--modal--pdf">
+        <img src="https://hellogrove.com/dist/img/marketing/guide-hero.png" class="guide--modal--image">
+        <h4 class="guide--modal--h4">2018 Personal Finance Guide</h4>
+        <p class="guide--modal--small">9 things you need to do to get your finances in order</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!--  -->
+      </div>
+    </div>
+  </div>
+</div>
+
+
+    <span id="scroll-container"></span>
+
+    
+        <footer class="grove--footer  marketing--footer    ">
+
+    <div class="grove--footer--bounds">
+        <div class="grid-25">
+            <h4 class="grove--mini--heading">Company</h4>
+            <ul class="grove--footer--nav">
+              <span data-component="MetricEvent"
+                  category="consideration" action="click about" label="all footer">
+                <li><a href="https://hellogrove.com/about">About us</a></li>
+              </span>
+
+              <span data-component="MetricEvent"
+                  category="consideration" action="click advisors" label="all footer">
+                <li><a href="https://hellogrove.com/advisors">Our advisors</a></li>
+              </span>
+
+              <span data-component="MetricEvent"
+                  category="consideration" action="click faq" label="all footer">
+                <li><a href="https://hellogrove.com/faq">FAQ</a></li>
+              </span>
+
+              <span data-component="MetricEvent"
+                  category="consideration" action="click careers" label="all footer">
+                <li><a href="https://hellogrove.com/about#careers">Careers</a></li>
+              </span>
+            </ul>
+        </div>
+
+        
+
+        <div class="grid-25">
+            <h4 class="grove--mini--heading">Legal</h4>
+            <ul class="grove--footer--nav">
+                <li><a href="https://hellogrove.com/privacypolicy">Privacy Policy</a></li>
+                <li><a href="https://hellogrove.com/termsofservice">Terms of Service</a></li>
+            </ul>
+        </div>
+
+        <div class="grid-25 grid-push-25">
+            <address>
+                <h4>Grove Advisors LLC</h4>
+                <p>542 Brannan St. #407<br />
+                    San Francisco, CA 94107
+                </p>
+            </address>
+        </div>
+
+        <div class="grid-100">
+            <section class="grove--footer--disclaimer">
+                <p>This material presented in this advertisement is for informational purposes only and should not be construed as investment advice. It is not a recommendation of, or an offer to sell or solicitation of an offer to buy, any particular security, strategy or investment product. Any analysis or discussion of investments, sectors or the market generally are based on current information, including from public sources, that we consider reliable, but we do not represent that any research or the information provided is accurate or complete, and it should not be relied on as such. Our views and opinions expressed in any online content are current at the time of publication and are subject to change.</p>
+            </section>
+        </div>
+    </div>
+
+</footer>
+    
+
+    
+</body>
+
+
+<script nonce="4950e046-fd46-40cd-8a8a-47825d6fc74d">
+  
+
+  window.grove = window.grove || {};
+  window.grove.reset_id = "";
+
+  window.grove.TODOS_DASHBOARD = true;
+  window.grove.ACCOUNT_SYNC_UI_LOGGING = false;
+  window.grove.SERVER_NAME = "hellogrove.com";
+  window.grove.PRERECORDED_WEBINAR_URL_TEMPLATE = "https://my.demio.com/rep/HY9eOtzPunvSt0YQ/[FNAME]/[EMAIL]";
+  window.grove.referrerCookieName = 'grovereferrer';
+
+  window.grove.SENTRY_PUBLIC_DSN = "https://f13682fa65bb4d0ca2179a4dc55d79b8@sentry.io/199902";
+  
+    window.grove.SENTRY_CONFIG = {"processors": ["raven.processors.RemovePostDataProcessor", "raven.processors.SanitizePasswordsProcessor"], "sample_rate": 1.0};
+  
+
+  
+
+  window.grove.profileType = "Client";
+</script>
+
+<script src="https://hellogrove.com/dist/js/marketing__710a57a5d75f729269112f7ad8f653aa.js"></script>
+
+<script nonce="4950e046-fd46-40cd-8a8a-47825d6fc74d">
+  window.intercomSettings = {
+    
+    app_id: "nuiposgi"
+  };
+</script>
+<script nonce="4950e046-fd46-40cd-8a8a-47825d6fc74d">(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/nuiposgi';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
+<script nonce="4950e046-fd46-40cd-8a8a-47825d6fc74d">
+  if (window.grove && window.grove.profile && window.grove.profile.isFrozen) {
+    if (window.Intercom && window.innerWidth > 450) {
+      window.Intercom('show');
+    }
+  }
+</script>
+</html>
---
+++ header.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Grove</title>
+    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width">
+    <meta name="description" content="Grove | Personalized Financial Planning">
+
+    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="https://hellogrove.com/dist/img/favicon/apple-touch-icon-57x57.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="https://hellogrove.com/dist/img/favicon/apple-touch-icon-60x60.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="https://hellogrove.com/dist/img/favicon/apple-touch-icon-72x72.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="https://hellogrove.com/dist/img/favicon/apple-touch-icon-76x76.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="https://hellogrove.com/dist/img/favicon/apple-touch-icon-114x114.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="https://hellogrove.com/dist/img/favicon/apple-touch-icon-120x120.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="https://hellogrove.com/dist/img/favicon/apple-touch-icon-144x144.png" />
+    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="https://hellogrove.com/dist/img/favicon/apple-touch-icon-152x152.png" />
+    <link rel="icon" type="image/png" href="https://hellogrove.com/dist/img/favicon/favicon-16x16.png" sizes="16x16" />
+    <link rel="icon" type="image/png" href="https://hellogrove.com/dist/img/favicon/favicon-32x32.png" sizes="32x32" />
+    <link rel="icon" type="image/png" href="https://hellogrove.com/dist/img/favicon/favicon-96x96.png" sizes="96x96" />
+    <link rel="icon" type="image/png" href="https://hellogrove.com/dist/img/favicon/favicon-128.png" sizes="128x128" />
+    <link rel="icon" type="image/png" href="https://hellogrove.com/dist/img/favicon/favicon-196x196.png" sizes="196x196" />
+    <meta name="application-name" content="Grove"/>
+    <meta name="msapplication-TileColor" content="#FFFFFF" />
+    <meta name="msapplication-TileImage" content="https://hellogrove.com/dist/img/favicon/mstile-144x144.png" />
+    <meta name="msapplication-square70x70logo" content="https://hellogrove.com/dist/img/favicon/mstile-70x70.png" />
+    <meta name="msapplication-square150x150logo" content="https://hellogrove.com/dist/img/favicon/mstile-150x150.png" />
+    <meta name="msapplication-square310x310logo" content="https://hellogrove.com/dist/img/favicon/mstile-310x310.png" />
+
+    <script nonce="4950e046-fd46-40cd-8a8a-47825d6fc74d">
+      window.eval = function() {
+        throw new Error();
+      };
+    </script>
+
+  <!-- Google Analytics -->
+
+  <style>.async-hide { opacity: 0 !important} </style>
+  <script nonce="4950e046-fd46-40cd-8a8a-47825d6fc74d">
+    (function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+    h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+    (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+    })(window,document.documentElement,'async-hide','dataLayer',4000,
+    {'GTM-WWS9VKB':true});
+  </script>
+
+  <script nonce="4950e046-fd46-40cd-8a8a-47825d6fc74d">
+    window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+    ga('create', "UA-82382093-3", 'auto');
+    
+      ga('require', "GTM-WWS9VKB");
+    
+    
+    ga('send', 'pageview');
+  </script>
+
+  <!-- Global site tag (gtag.js) - Google AdWords: 757-368-3766 -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=AW-847562266"></script>
+  <script nonce="4950e046-fd46-40cd-8a8a-47825d6fc74d">
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', "AW-847562266");
+  </script>
+  <script nonce="4950e046-fd46-40cd-8a8a-47825d6fc74d">
+    function gtag_report_lead_conversion() {
+      gtag('event', 'conversion', {
+        'send_to': 'AW-847562266/DPLmCJf8pHYQmoyTlAM'
+      });
+    }
+    function gtag_report_reservation_conversion() {
+      gtag('event', 'conversion', {
+        'send_to': 'AW-847562266/lBcLCPGkj38QmoyTlAM'
+      });
+    }
+    function gtag_report_membership_conversion() {
+      gtag('event', 'conversion', {
+        'send_to': 'AW-847562266/PdkdCOi8qn8QmoyTlAM'
+      });
+    }
+    function fb_report_lead_conversion() {
+      window.fbq('track', 'Lead');
+    }
+    function fb_report_reservation_conversion(amount) {
+      window.fbq('track', 'Purchase', {
+        value: amount,
+        currency: 'USD',
+        content_ids: 'Reserve',
+      });
+    }
+    function fb_report_membership_conversion(amount) {
+      window.fbq('track', 'Purchase', {
+        value: amount,
+        currency: 'USD',
+        content_ids: 'Membership',
+      });
+    }
+  </script>
+
+  <script async src='https://www.google-analytics.com/analytics.js'></script>
+  <!-- End Google Analytics -->
+
+  <!-- Facebook Pixel Code -->
+  <script nonce="4950e046-fd46-40cd-8a8a-47825d6fc74d">
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', "1558734264149077");
+    fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=1558734264149077&ev=PageView&noscript=1"
+  /></noscript>
+  <!-- End Facebook Pixel Code -->
+
+  
+
+  
+    <link rel="stylesheet" href="https://hellogrove.com/dist/css/grove__094fc46e04fdd06d8a5bf6e8b9eacea9.css">
+  
+
+</head>
+<body not-loaded 
+  class="marketing  "
+>
+
+    
+    
+
+    
+        <div class="marketing--fixed">
+  <div id="messages_container"></div>
+    <header class="marketing--header">
+        <div class="marketing--bounds">
+            <div class="grid-100 centered">
+                <span data-component="MetricEvent"
+                      category="consideration" action="click hamburger" label="all navigation">
+                    <a class="marketing--mobile--open"><span class="icon-menu"></span></a>
+                </span>
+
+                <span data-component="MetricEvent"
+                      category="consideration" action="click grove icon" label="all navigation">
+                    <a href="https://hellogrove.com/" class="marketing--logo">
+                        <img src="https://hellogrove.com/dist/img/logo.svg" alt="Grove">
+                    </a>
+                </span>
+
+                <nav>
+                    <div class="marketing--mobile--nav">
+                        <a class="marketing--mobile--close"><span class="icon-x"></span></a>
+                        <ul class="marketing--primary--nav">
+                            <li>
+                              <span data-component="MetricEvent"
+                                  category="consideration" action="click about" label="all navigation">
+                                <a class="marketing--nav--link" href="https://hellogrove.com/about">About us</a>
+                              </span>
+                            </li>
+                            <li>
+                              <span data-component="MetricEvent"
+                                  category="consideration" action="click pricing" label="all navigation">
+                                <a class="marketing--nav--link" href="https://hellogrove.com/pricing">Pricing</a>
+                              </span>
+                            </li>
+                            <li>
+                              <span data-component="MetricEvent"
+                                  category="consideration" action="click advisors" label="all navigation">
+                                <a class="marketing--nav--link" href="https://hellogrove.com/advisors">Our advisors</a>
+                              </span>
+                            <li>
+                              <span data-component="MetricEvent"
+                                  category="consideration" action="click faq" label="all navigation">
+                                <a class="marketing--nav--link" href="https://hellogrove.com/faq">FAQ</a>
+                              </span>
+                            </li>
+                            
+                              <li>
+                                <span data-component="MetricEvent"
+                                    category="assessment" action="click get started" label="all navigation">
+                                  <a class="marketing--nav--link" href="https://hellogrove.com/checkup">Free checkup</a>
+                                </span>
+                              </li>
+                            
+                        </ul>
+
+                        <ul class="marketing--secondary--nav">
+                            
+                            <li id="grove-signin-modal-container" data-component="SigninModal">
+                              <a class="marketing--nav--link">Sign in</a>
+                            </li>
+
+                            
+                              
+                                  <li class="grove--getting-started"
+                                      data-component="GettingStarted"
+                                      text="Get started"
+                                      
+                                      action="click get started"
+                                      label="home navigation"
+                                          home="true"
+                                        >
+                                    <a class="button-primary">Get started</a>
+                                  </li>
+                              
+                            
+                            
+                        </ul>
+                    </div>
+                </nav>
+
+
+            </div>
+        </div>
+    </header>
+</div>
+    
+
+    <div class="	">
